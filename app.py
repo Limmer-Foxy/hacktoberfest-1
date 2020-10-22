3c0c1cc72ac0ae51d4b7e28f36a181dfca66a6fb
Limmer-Foxy <69829482+Limmer-Foxy@users.noreply.github.com>
2020-10-22 17:55:01 +0800

    Update app.py
    Changed the code a little and made some changes to the text to make it better :)

--- app.py
+++ app.py
@@ -1,43 +1,51 @@
-list_of_words = description.split()
+print("Title of program: Nehemaiah's Encouragement bot")
+print()
+while True:
+  description = input("Could you describe how you feel about your exams?")
 
-feelings_list = []
-encouragement_list = []
-counter = 0
+  list_of_words = description.split()
 
-for each_word in list_of_words:
-
+  feelings_list = []
+  encouragement_list = []
+  counter = 0
+  
+  for each_word in list_of_words:
+    
     if each_word == "bad":
         feelings_list.append("bad")
-        encouragement_list.append("it's alright,you've already done your best")
+        encouragement_list.append("it's alright, you've already done your best")
         counter += 1
     if each_word == "good":
         feelings_list.append("good")
-        encouragement_list.append("you should keep up the good work!")
+        encouragement_list.append("you should keep up the good work")
         counter += 1
     if each_word == "moderate":
         feelings_list.append("moderate")
-        encouragement_list.append("to have more confidence in yourself! You can do it!")
+        encouragement_list.append("you can do it! Also, remember to have more confidence in yourself")
         counter += 1
 
-if counter == 0:
-    output = "Sorry I don't really understand. Please use different words?"
-
-elif counter == 1:
-    output = "It seems that you are feeling quite " + feelings_list[0] + ". However, do remember that "+ encouragement_list[0] + "! Hope you feel better :)"
-
-else:
-    feelings = ""
-    for i in range(len(feelings_list)-1):
-        feelings += feelings_list[i] + ", "
-        feelings += "and " + feelings_list[-1]
+  if counter == 0:
     
-        encouragement = ""
-        for j in range(len(encouragement_list)-1):
-            encouragement += encouragement_list[i] + ", "
-            encouragement += "and " + encouragement_list[-1]
+      output = "Sorry I don't really understand. Please use different words."
+
+  elif counter == 1:
+    
+      output = "It seems that you are feeling quite " + feelings_list[0] + ". However, do remember that "+ encouragement_list[0] + "! Hope you feel better :)"  
+
+  else:
+
+    feelings = ""    
+    for i in range(len(feelings_list)-1):
+      feelings += feelings_list[i] + ", "
+    feelings += "and " + feelings_list[-1]
+    
+    encouragement = ""    
+    for j in range(len(encouragement_list)-1):
+      encouragement += encouragement_list[i] + ", "
+    encouragement += "and " + encouragement_list[-1]
 
     output = "It seems that you are feeling quite " + feelings + ". Please always remember "+ encouragement + "! Hope you feel better :)"
 
-print()
-print(output)
-print()
+  print()
+  print(output)
+  print()
